d0384ce3e8e9dd80a5cbcd720b54e123b5870c87
Stanish <macios1999@gmail.com>
2016-05-02 14:53:15 +0200

    For Me U Got Stajla

--- game.cpp
+++ game.cpp
@@ -32,29 +32,30 @@ void Game::run()
     #else
     world = new b2World(Gravity, 0);
     #endif
-SceneManager::init();
+
+    SceneManager::init();
+
     world->SetContactListener(&contactlistener);
-createWindowAndStuff();
+
+    createWindowAndStuff();
+
     engine.add(std::shared_ptr<System>(new WindowSystem(window,views)));
     engine.add(std::shared_ptr<System>(new PuzzleSystem(container)));
     engine.add(std::shared_ptr<System>(new SceneSystem(container)));
     engine.add(std::shared_ptr<System>(new TestSystem(window, this)));
     engine.add(std::shared_ptr<System>(new MenuSystem(&window, &container)));
     engine.add(std::shared_ptr<System>(new PlayerSystem(&window, &container, this, &views)));
-     engine.add(std::shared_ptr<System>(new WeaponSystem(&container,world)));
+    engine.add(std::shared_ptr<System>(new WeaponSystem(&container,world)));
     engine.add(std::shared_ptr<System>(new BulletSystem(&container,world)));
     engine.add(std::shared_ptr<System>(new PhysicsSystem(world,container)));
     engine.add(std::shared_ptr<System>(new ZSystem(&window, &container, &views)));
     engine.add(std::shared_ptr<System>(new SoundSystem()));
     engine.add(std::shared_ptr<System>(new BotSystem(&container,world)));
     engine.add(std::shared_ptr<System>(new ParticleSystem(&container)));
-
-        engine.add(std::shared_ptr<System>(new DebugDrawSystem(world, &window, &views)));
+    engine.add(std::shared_ptr<System>(new DebugDrawSystem(world, &window, &views)));
     engine.add(std::shared_ptr<System>(new RemoveSystem(&container)));
 
-
     EventChannel chan;
-
 
     loadAssets("assets.xml");
     createMenus();
@@ -97,6 +98,10 @@ void Game::createMenus() {
 		gui = Stylesheets.Get("text");
 		gui->font = fonts.Get(gui->fontName);
 
+		GuiStyle* gui2;
+		gui2 = Stylesheets.Get("no_highlight");
+		gui2->font = fonts.Get(gui2->fontName);
+
 		int ID = container.getUniqueID();
 		container.createComponent<Transform>(ID);
 
@@ -116,10 +121,16 @@ void Game::createMenus() {
 		MenuFactory::get().addScreen(*container.getComponent<Menu>(ID), "options");
 		MenuFactory::get().addGui(*
                             container.getComponent<Menu>(ID), "main", sf::Vector2f(0, 64.0 * 1.5), sf::Vector2f(xsize, 64), 4, false, *gui,
-		{ std::make_pair("Start", "start"), std::make_pair("Options", "options_msg"), std::make_pair("Quit", "quit_msg") });
+		{ std::make_pair("Play", "start"), std::make_pair("Options", "options_msg"), std::make_pair("Quit", "quit_msg") });
 		MenuFactory::get().addGui(*
                             container.getComponent<Menu>(ID), "options", sf::Vector2f(0, 64.0 * 1), sf::Vector2f(xsize, 64), 4, false, *gui,
 		{ std::make_pair("Reset Game Saves", "reset_msg"),std::make_pair("Back", "back_msg") });
+
+		MenuFactory::get().addGui(*
+                            container.getComponent<Menu>(ID), "options", sf::Vector2f(0, 512.0 * 1), sf::Vector2f(xsize, 64), 4, false, *gui2,
+		{ std::make_pair("To change the volume edit the config file and restart the game", "ok") });
+
+
 		MenuFactory::get().addConnection(*container.getComponent<Menu>(ID), "main", "options_msg", "options");
 		MenuFactory::get().addConnection(*container.getComponent<Menu>(ID), "options", "back_msg", "main");
 		MenuFactory::get().addConnection(*container.getComponent<Menu>(ID), "main", "start", "options");
